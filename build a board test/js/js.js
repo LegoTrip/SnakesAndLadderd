632794e28dc18f68864313fabc47433520dd0998
LegoTrip <mailjaredwoods@gmail.com>
2017-04-10 17:40:43 +0100

    Trying to find a better way to add different IDs to each block

--- build a board test/js/js.js
+++ build a board test/js/js.js
@@ -1,6 +1,6 @@
 
 
-var numOfRows = 10;
+var numOfRows = 2;
 var numOfColumns = numOfRows;
 
 document.getElementById('board-wrapper').style.width = ( numOfRows * 129)+'px';
@@ -10,7 +10,7 @@ var arrayDiv = new Array();
 for (var j = 0; j < numOfColumns; j++) {
   for (var i = 0; i < numOfRows; i++) {
     arrayDiv[i] = document.createElement('div');
-    arrayDiv[i].id = 'block' + i;
+    //arrayDiv[i].id = 'block' + i;
     arrayDiv[i].className = 'block';
 
     document.getElementById('board-wrapper').appendChild(arrayDiv[i]);
@@ -18,5 +18,10 @@ for (var j = 0; j < numOfColumns; j++) {
   document.getElementById('board-wrapper').appendChild(arrayDiv[j]);
 }
 
-
-
+var elements = document.getElementsByClassName("block");
+var id = '';
+for(var i=0; i<elements.length; i++) {
+	//alert("here");
+    document.querySelector('div.block').id = 'block6'; //only adding to first??
+}
+document.write(id);
