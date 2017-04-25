39733b546f51276760e3fe2e9c611eb3be4d2944
LegoTrip <Jared_Woods@dennis.co.uk>
2017-04-25 18:03:22 +0100

    MUCH better way of creating the blocks; adding ID’s in reverse order

--- build a board test/js/js.js
+++ build a board test/js/js.js
@@ -8,27 +8,25 @@ if (isNaN(num1) || (num1) > 12) {
 //SET THE BOARD BLOCK AMOUNT AND SIZE THE FUCKER
 var numOfRows = num1;
 var numOfColumns = numOfRows;
+var totalNumOfBlocks = (numOfRows * numOfColumns)
+alert(totalNumOfBlocks);
 document.getElementById('board-wrapper').style.width = ( numOfRows * 129)+'px';
 
 //CREATE BOARD
 var arrayBoardBlocks = new Array();
-for (var columnBlocks = 0; columnBlocks < numOfColumns; columnBlocks++) {
-  //MAKE THE ROWS AS MANY TIMES AS THE COLUMNS
-  for (var rowBlocks = 0; rowBlocks < numOfRows; rowBlocks++) {
-    arrayBoardBlocks[rowBlocks] = document.createElement('div');
-    arrayBoardBlocks[rowBlocks].className = 'block';
-    document.getElementById('board-wrapper').appendChild(arrayBoardBlocks[rowBlocks]);
-  }
+for (var columnBlocks = 0; columnBlocks < totalNumOfBlocks; columnBlocks++) {
+  arrayBoardBlocks[columnBlocks] = document.createElement('div');
+  arrayBoardBlocks[columnBlocks].className = 'block';
   document.getElementById('board-wrapper').appendChild(arrayBoardBlocks[columnBlocks]);
 }
 
 //GIVE EACH BLOCK IT'S OWN CLASS
 var elements = document.getElementsByClassName("block");
-for(var blockNumber=0; blockNumber<elements.length; blockNumber++) {
-    var elem = elements[blockNumber];
-    elem.id = "block" + [blockNumber];
+var num = totalNumOfBlocks;
+for (var blockNumber = 0, n = true?num-1:0, d = true?-1:1; blockNumber < num; blockNumber++, n+=d) {
+  var elem = elements[blockNumber];
+  elem.id = "block" + [n];
 }
-
 
 
 
