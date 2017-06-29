8e2491d4e6c0ed64eab029d4dc990639b2e3a2fa
Jared Woods <Jared_Woods@dennis.co.uk>
2017-06-29 17:50:51 +0100

    cleaning up, setting arrays as globals, removing commas, separate functions for giving IDs to blocks and reversing rows

--- build a board test/js/js.js
+++ build a board test/js/js.js
@@ -11,77 +11,40 @@ var numOfColumns = numOfRows;
 var totalNumOfBlocks = (numOfRows * numOfColumns)
 document.getElementById('board-wrapper').style.width = ( numOfRows * 129)+'px';
 
-var arrayBoardBlocks = new Array();
-var blockCreation = '';
+//CODE FOR BLOCK CREATION
+var blockCreation = '<div class="block"></div>';
+
+//GLOBAL EMPTY ARRAYS
+var rowArray  = [];
+var columnArray  = [];
 
 //CREATE BLOCK 
 function createBlock() {  
-  arrayBoardBlocks[blockCreation] = document.createElement('div');
-  arrayBoardBlocks[blockCreation].className = 'block';
-  document.getElementById('board-wrapper').appendChild(arrayBoardBlocks[blockCreation]);
+  rowArray.push(blockCreation);
 }
 
 //CREATE ROW
-function createRow() {  
-  // for (var blockCreation = 0; blockCreation < numOfRows; blockCreation++) {
-  //   createBlock();
-  //   console.log(blockCreation);
-  // }  
-  var rowArray  = [];
+function createRow() {   
   for (var rowCreation = 0; rowCreation < numOfRows; rowCreation++) {
-    //rowArray.push('<div id="block' + rowCreation + '" class="block"></div>');
-    rowArray.push('<div class="block"></div>');
+    createBlock();
   }  
-  var rowShove = rowArray.join();
+  var rowShove = rowArray.join("");
 
-
-  var columnArray  = [];
+  
   for (var columnCreation = 0; columnCreation < numOfColumns; columnCreation++) {
     columnArray.push('<div class="row" id="row' + columnCreation + '">' + rowShove + '</div>');
   }
-  var columnShove = columnArray.join();
+  var columnShove = columnArray.join("");
 
   document.getElementById("board-wrapper").innerHTML = columnShove;
 
-
-  // ADD A CLASS TO EVERY SECOND ROW TO REVERSE ORDER
-  numOfRows2 = (numOfRows) - 1;
-  for(var i = numOfRows2; i > -1; i--) {    
-    var everysecongrow = i % 2 === 0;
-    alert(everysecongrow);
-    if (everysecongrow) {
-      var leftrow = "row" + i;
-      var d = document.getElementById(leftrow);
-      d.className += " leftie";
-    }
-  }
-
-
+  blockIDing();
 }
-
-
-
-
-
 
 //CREATE COLUMNS
 function createColumn() {  
-  //for (var columnCreation = 0; columnCreation < numOfColumns; columnCreation++) {
-    createRow();
-  //}
+  createRow();
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 //CREATE BOARD
 function createBoard() {
@@ -90,13 +53,29 @@ function createBoard() {
 createBoard();
 
 //GIVE EACH BLOCK IT'S OWN ID
-var elements = document.getElementsByClassName("block");
-var num = totalNumOfBlocks;
-for (var blockNumber = 0, n = true?num-1:0, d = true?-1:1; blockNumber < num; blockNumber++, n+=d) {
-  var elem = elements[blockNumber];
-  elem.id = "block" + [n];
-  var generateHere = document.getElementById(elem.id);
-  generateHere.innerHTML = '<p>' + [n] + '</p>';
+function blockIDing() {   
+  var elements = document.getElementsByClassName("block");
+  var num = totalNumOfBlocks;
+  for (var blockNumber = 0, n = true?num-1:0, d = true?-1:1; blockNumber < num; blockNumber++, n+=d) {
+    var elem = elements[blockNumber];
+    elem.id = "block" + [n];
+    var generateHere = document.getElementById(elem.id);
+    generateHere.innerHTML = '<p>' + [n] + '</p>';    
+  }
+  secondRowClass();
+}
+
+// ADD A CLASS TO EVERY SECOND ROW TO REVERSE ORDER
+function secondRowClass() {   
+  numOfRows2 = (numOfRows) - 1;
+  for(var i = numOfRows2; i > -1; i--) {    
+    var everysecondrow = i % 2 === 0;
+    if (everysecondrow) {
+      var leftrow = "row" + i;
+      var leftrowgrab = document.getElementById(leftrow);
+      leftrowgrab.className = "row leftie";
+    }
+  }
 }
 
 
